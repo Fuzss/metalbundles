d1784be8015c41dcd4517983c0bfba28001c3bf9
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-12-24 09:30:11 +0100

    remove vanilla texture files

--- 1.21.3/Common/src/main/java/fuzs/metalbundles/data/client/ModModelProvider.java
+++ 1.21.3/Common/src/main/java/fuzs/metalbundles/data/client/ModModelProvider.java
@@ -1,12 +1,21 @@
 package fuzs.metalbundles.data.client;
 
+import com.google.gson.JsonElement;
 import fuzs.metalbundles.init.ModRegistry;
 import fuzs.puzzleslib.api.client.data.v2.AbstractModelProvider;
 import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
 import net.minecraft.data.models.ItemModelGenerators;
+import net.minecraft.data.models.model.ModelTemplate;
 import net.minecraft.data.models.model.ModelTemplates;
+import net.minecraft.data.models.model.TextureMapping;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BundleItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class ModModelProvider extends AbstractModelProvider {
 
@@ -16,16 +25,36 @@ public class ModModelProvider extends AbstractModelProvider {
 
     @Override
     public void addItemModels(ItemModelGenerators builder) {
-        this.createMetalBundleItem(builder, ModRegistry.COPPER_BUNDLE_ITEM.value());
-        this.createMetalBundleItem(builder, ModRegistry.IRON_BUNDLE_ITEM.value());
-        this.createMetalBundleItem(builder, ModRegistry.GOLDEN_BUNDLE_ITEM.value());
-        this.createMetalBundleItem(builder, ModRegistry.DIAMOND_BUNDLE_ITEM.value());
-        this.createMetalBundleItem(builder, ModRegistry.NETHERITE_BUNDLE_ITEM.value());
+        this.createMetalBundleItem(builder, ModRegistry.COPPER_BUNDLE_ITEM.value(), Items.ORANGE_BUNDLE);
+        this.createMetalBundleItem(builder, ModRegistry.IRON_BUNDLE_ITEM.value(), Items.LIGHT_GRAY_BUNDLE);
+        this.createMetalBundleItem(builder, ModRegistry.GOLDEN_BUNDLE_ITEM.value(), Items.YELLOW_BUNDLE);
+        this.createMetalBundleItem(builder, ModRegistry.DIAMOND_BUNDLE_ITEM.value(), Items.CYAN_BUNDLE);
+        this.createMetalBundleItem(builder, ModRegistry.NETHERITE_BUNDLE_ITEM.value(), Items.BLACK_BUNDLE);
     }
 
     private void createMetalBundleItem(ItemModelGenerators builder, Item item) {
         builder.generateFlatItem(item, ModelTemplates.FLAT_ITEM);
         generateFlatItem(((BundleItem) item).openBackModel(), ModelTemplates.FLAT_ITEM, builder.output);
         generateFlatItem(((BundleItem) item).openFrontModel(), ModelTemplates.FLAT_ITEM, builder.output);
+    }
+
+    private void createMetalBundleItem(ItemModelGenerators builder, Item item, Item itemTemplate) {
+        builder.generateFlatItem(item, itemTemplate, ModelTemplates.FLAT_ITEM);
+        this.generateBundleItem(item, itemTemplate, BundleItem::openBackModel, builder);
+        this.generateBundleItem(item, itemTemplate, BundleItem::openFrontModel, builder);
+    }
+
+    private void generateBundleItem(Item item, Item itemTemplate, Function<BundleItem, ResourceLocation> modelGetter, ItemModelGenerators builder) {
+        generateFlatItem(modelGetter.apply((BundleItem) item),
+                modelGetter.apply((BundleItem) itemTemplate),
+                ModelTemplates.FLAT_ITEM,
+                builder.output);
+    }
+
+    @Deprecated(forRemoval = true)
+    public static ResourceLocation generateFlatItem(ResourceLocation resourceLocation, ResourceLocation layerResourceLocation, ModelTemplate modelTemplate, BiConsumer<ResourceLocation, Supplier<JsonElement>> modelOutput) {
+        return modelTemplate.create(decorateItemModelLocation(resourceLocation),
+                TextureMapping.layer0(decorateItemModelLocation(layerResourceLocation)),
+                modelOutput);
     }
 }
