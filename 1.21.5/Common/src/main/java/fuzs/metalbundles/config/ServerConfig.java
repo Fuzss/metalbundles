8d53f528b2bd54ae58e739924a4e45cbd12d999d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-04-17 13:38:01 +0200

    prepare 1.21.5 port

---
+++ 1.21.5/Common/src/main/java/fuzs/metalbundles/config/ServerConfig.java
@@ -0,0 +1,22 @@
+package fuzs.metalbundles.config;
+
+import fuzs.puzzleslib.api.config.v3.Config;
+import fuzs.puzzleslib.api.config.v3.ConfigCore;
+
+public class ServerConfig implements ConfigCore {
+    @Config(description = "Multiplier for the base bundle capacity of 64 items.")
+    @Config.IntRange(min = 1)
+    public int copperCapacityMultiplier = 2;
+    @Config(description = "Multiplier for the base bundle capacity of 64 items.")
+    @Config.IntRange(min = 1)
+    public int ironCapacityMultiplier = 4;
+    @Config(description = "Multiplier for the base bundle capacity of 64 items.")
+    @Config.IntRange(min = 1)
+    public int goldenCapacityMultiplier = 8;
+    @Config(description = "Multiplier for the base bundle capacity of 64 items.")
+    @Config.IntRange(min = 1)
+    public int diamondCapacityMultiplier = 16;
+    @Config(description = "Multiplier for the base bundle capacity of 64 items.")
+    @Config.IntRange(min = 1)
+    public int netheriteCapacityMultiplier = 32;
+}
---
+++ 1.21.5/Common/src/main/java/fuzs/metalbundles/data/client/ModModelProvider.java
@@ -0,0 +1,101 @@
+package fuzs.metalbundles.data.client;
+
+import fuzs.metalbundles.MetalBundles;
+import fuzs.metalbundles.init.ModRegistry;
+import fuzs.puzzleslib.api.client.data.v2.AbstractModelProvider;
+import fuzs.puzzleslib.api.client.data.v2.models.ModelLocationHelper;
+import fuzs.puzzleslib.api.client.data.v2.models.ModelTemplateHelper;
+import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ItemModelUtils;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.renderer.item.ItemModel;
+import net.minecraft.client.renderer.item.properties.conditional.BundleHasSelectedItem;
+import net.minecraft.client.renderer.item.properties.select.DisplayContext;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.*;
+
+import java.util.Map;
+
+public class ModModelProvider extends AbstractModelProvider {
+
+    public ModModelProvider(DataProviderContext context) {
+        super(context);
+    }
+
+    @Override
+    public void addItemModels(ItemModelGenerators itemModelGenerators) {
+        this.createMetalBundleItem(itemModelGenerators,
+                ModRegistry.COPPER_BUNDLE_ITEM.value(),
+                ModRegistry.COPPER_BUNDLE_ITEMS,
+                "copper_bundle_string",
+                "copper_bundle_open_string");
+        this.createMetalBundleItem(itemModelGenerators,
+                ModRegistry.IRON_BUNDLE_ITEM.value(),
+                ModRegistry.IRON_BUNDLE_ITEMS,
+                "iron_bundle_string",
+                "iron_bundle_open_string");
+        this.createMetalBundleItem(itemModelGenerators,
+                ModRegistry.GOLDEN_BUNDLE_ITEM.value(),
+                ModRegistry.GOLDEN_BUNDLE_ITEMS,
+                "golden_bundle_string",
+                "golden_bundle_open_string");
+        this.createMetalBundleItem(itemModelGenerators,
+                ModRegistry.DIAMOND_BUNDLE_ITEM.value(),
+                ModRegistry.DIAMOND_BUNDLE_ITEMS,
+                "diamond_bundle_string",
+                "diamond_bundle_open_string");
+        this.createMetalBundleItem(itemModelGenerators,
+                ModRegistry.NETHERITE_BUNDLE_ITEM.value(),
+                ModRegistry.NETHERITE_BUNDLE_ITEMS,
+                "netherite_bundle_string",
+                "netherite_bundle_open_string");
+    }
+
+    public final void createMetalBundleItem(ItemModelGenerators itemModelGenerators, Item bundleItem, Map<DyeColor, Holder.Reference<Item>> bundleItems, String string, String openString) {
+        ResourceLocation stringResourceLocation = MetalBundles.id(string);
+        ResourceLocation openStringResourceLocation = MetalBundles.id(openString);
+        this.createMetalBundleItem(itemModelGenerators,
+                bundleItem,
+                Items.BUNDLE,
+                stringResourceLocation,
+                openStringResourceLocation);
+        for (Map.Entry<DyeColor, Holder.Reference<Item>> entry : bundleItems.entrySet()) {
+            this.createMetalBundleItem(itemModelGenerators,
+                    entry.getValue().value(),
+                    BundleItem.getByColor(entry.getKey()),
+                    stringResourceLocation,
+                    openStringResourceLocation);
+        }
+    }
+
+    /**
+     * Partially copied from {@link ItemModelGenerators#generateBundleModels(Item)}.
+     */
+    public final void createMetalBundleItem(ItemModelGenerators itemModelGenerators, Item bundleItem, Item vanillaBundleItem, ResourceLocation stringResourceLocation, ResourceLocation openStringResourceLocation) {
+        ResourceLocation resourceLocation = ModelTemplateHelper.generateLayeredItem(bundleItem,
+                ModelLocationHelper.getItemLocation(vanillaBundleItem),
+                stringResourceLocation,
+                itemModelGenerators.modelOutput);
+        ResourceLocation openBackResourceLocation = ModelTemplateHelper.generateFlatItem(ModelLocationHelper.getItemLocation(
+                        bundleItem).withSuffix("_open_back"),
+                ModelLocationHelper.getItemLocation(vanillaBundleItem).withSuffix("_open_back"),
+                ModelTemplates.FLAT_ITEM,
+                itemModelGenerators.modelOutput);
+        ResourceLocation openFrontResourceLocation = ModelTemplateHelper.generateLayeredItem(ModelLocationHelper.getItemLocation(
+                        bundleItem).withSuffix("_open_front"),
+                ModelLocationHelper.getItemLocation(vanillaBundleItem).withSuffix("_open_front"),
+                openStringResourceLocation,
+                itemModelGenerators.modelOutput);
+        ItemModel.Unbaked unbaked = ItemModelUtils.plainModel(resourceLocation);
+        ItemModel.Unbaked unbaked2 = ItemModelUtils.composite(ItemModelUtils.plainModel(openBackResourceLocation),
+                new net.minecraft.client.renderer.item.BundleSelectedItemSpecialRenderer.Unbaked(),
+                ItemModelUtils.plainModel(openFrontResourceLocation));
+        ItemModel.Unbaked unbaked3 = ItemModelUtils.conditional(new BundleHasSelectedItem(), unbaked2, unbaked);
+        itemModelGenerators.itemModelOutput.accept(bundleItem,
+                ItemModelUtils.select(new DisplayContext(),
+                        unbaked,
+                        ItemModelUtils.when(ItemDisplayContext.GUI, unbaked3)));
+    }
+}
